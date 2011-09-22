1d86d043eab7ab42180370afee17e382d9b99005
Tjerk Santegoeds <tsan@tdias.com>
2011-09-22 18:06:03 +0100

    Small change

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python 
 
-from nose.tools import *
+from os import path
 from pprint import pprint
+from nose.tools import *
 
 from BFService import _util
 
 
 def test_uncompress_market_prices():
+    data_dir = path.join(path.dirname(__file__), "data")
     prices = open("tests/data/compressed_prices").read()
     pprint(_util.uncompress_market_prices(prices))
-
 
