a97af944b6aca3ea539f3137a10e80ea6670d502
Tjerk Santegoeds <tsan@tdias.com>
2011-10-20 13:10:33 +0100

    Test configuration

--- tests/test_session.py
+++ tests/test_session.py
@@ -1,1 +1,11 @@
 
+
+def pytest_funcarg__session(request):
+    #import pdb; pdb.set_trace()
+    user = request.config.inicfg.get("betfair_user")
+    password = request.config.inicfg.get("betfair_password")
+    return user, password
+
+
+def test_example(session):
+    assert session[0] == "santegoeds"
